33e1437f11fc9c05f64366378c70292800e47dfc
Luis Camacho <luiscamacho@gmail.com>
2017-04-26 07:58:20 -0400

    Added Events to landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
- 
-var animatePoints = function() {
-     var points = document.getElementsByClassName('point');
- 
+var pointsArray = document.getElementsByClassName('point'); 
+var animatePoints = function(points) {
+      
     for(i=0; i <points.length; i++){
          points[i].style.opacity = 1;
          points[i].style.transform = "scaleX(1) translateY(0)";
@@ -9,3 +8,20 @@ var animatePoints = function() {
          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
  };
+
+window.onload = function () {
+    
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+          animatePoints(pointsArray);
+      }
+    })
+}
+
