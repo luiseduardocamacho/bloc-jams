4b5cea9d45d9ea6d35136db433136e0b9c7d352f
Luis Camacho <luiscamacho@gmail.com>
2017-06-30 17:58:53 -0400

    Added getSong and getSongNumberCell and refactored other classes inside album.js

--- scripts/album.js
+++ scripts/album.js
@@ -38,14 +38,13 @@ var createSongRow = function(songNumber, songName, songLength){
       var songNumber = parseInt($(this).attr('data-song-number'));
       if (currentlyPlayingSongNumber !== null) {
 		      // Revert to song number for currently playing song because user started playing new song.
-		      var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		      var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
 		        currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	     }
 	     if (currentlyPlayingSongNumber !== songNumber) {
 		      // Switch from Play -> Pause button to indicate new song is playing.
 		      $(this).html(pauseButtonTemplate);
-		      currentlyPlayingSongNumber = songNumber;
-          currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+		      setSong(songNumber);
           updatePlayerBarSong();
 	       }
       else if (currentlyPlayingSongNumber === songNumber) {
@@ -124,8 +123,8 @@ var nextSong = function() {
     // Update the Player Bar information
     updatePlayerBarSong();
 
-    var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
-    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+    var $lastSongNumberCell = getSongNumberCell(lastSongNumber);
 
     $nextSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
@@ -152,8 +151,8 @@ var previousSong = function() {
 
     $('.main-controls .play-pause').html(playerBarPauseButton);
 
-    var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
-    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+    var $lastSongNumberCell = getSongNumberCell(lastSongNumber);
 
     $previousSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
@@ -173,6 +172,15 @@ var updatePlayerBarSong = function(){
   $(".currently-playing .artist-song-mobile").text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
   $('.main-controls .play-pause').html(playerBarPauseButton);
 };
+
+var setSong = function(songNumber){
+  currentlyPlayingSongNumber = parseInt(songNumber);
+  currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+}
+
+var getSongNumberCell = function(number){
+  return $('.song-item-number[data-song-number="' + number + '"]');
+}
 
 $(document).ready(function() {
   setCurrentAlbum(albumCabrera);
